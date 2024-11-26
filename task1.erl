fa57553fdc52548e7e9861937320ab38559febb7
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-11-26 20:15:40 +0000

    Added package creation loop and syncronization

--- task1.erl
+++ task1.erl
@@ -1,31 +1,45 @@
 -module(task1).
--export([start/0, truck_spawner/1]).
+-export([start/0, truck_spawner/1, package_creator/0]).
 
 
 -define (TruckLimit, 30).
+-define (TruckSpawnDelay, 300).
+
+-define (PackageCreationDelay, 250).
+
+
 
 spawn_truck(Id) ->
     io:fwrite("Truck Spawner: Creating truck with id ~w~n", [Id]),
-    timer:sleep(300),
+    timer:sleep(?TruckSpawnDelay),
     if 
         Id < ?TruckLimit -> spawn_truck(Id + 1);
         true -> io:fwrite("Truck Spawner: No more trucks~n", [])
     end.
 
-
-
-truck_spawner(StartPid) ->
+truck_spawner(PackageCreatorId) ->
 
     TruckSpawnerId = self(),
 
     io:fwrite("Truck spawner starting... Proccess: ~p~n", [TruckSpawnerId]),
 
-    spawn_truck(1).
+    spawn_truck(1), %start loop of spawning trucks till we have no more
+
+    PackageCreatorId ! stop.
     
 
 
+create_package(Id) ->
+    io:fwrite("Package creator: Creating package with id ~w~n", [Id]),
+    receive
+        stop ->  io:fwrite("Package creator: Received message to stop, stopping...~n", [])
+        after ?PackageCreationDelay -> create_package(Id + 1)
+    end.
+
+
 package_creator() -> 
-    io:fwrite("Package creator starting... Proccess: ~p~n", [self()]).
+    io:fwrite("Package creator starting... Proccess: ~p~n", [self()]),
+    create_package(1).
 
 
 
@@ -39,5 +53,7 @@ start() ->
 
     io:fwrite("Started main function... Proccess: ~p~n", [StartPid]),
 
-    TruckSpawnerPid = spawn(task1, truck_spawner, [StartPid]).
+    PackageCreatorId = spawn(?MODULE, package_creator, []),
+
+    TruckSpawnerPid = spawn(?MODULE, truck_spawner, [PackageCreatorId]).
     
