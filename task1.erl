e8ba52cab80f30530e69ae3381e2e428b466e003
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-11-26 19:23:39 +0000

    Added basis of trucker spawner

--- task1.erl
+++ task1.erl
@@ -1,15 +1,28 @@
 -module(task1).
--export([start/0]).
+-export([start/0, truck_spawner/1]).
 
-truck_spawner() ->
-    io:fwrite("Truck spawner starting").
+truck_spawner(StartPid) ->
+
+    TruckSpawnerId = self(),
+
+    io:fwrite("Truck spawner starting... Proccess: ~p~n", [TruckSpawnerId]),
+    StartPid ! {done, TruckSpawnerId }.
     
 package_creator() ->
-    io:fwrite("Package creator starting").
+    io:fwrite("Package creator starting... Proccess: ~p~n", [self()]).
     
 conveyor_belt() ->
-    io:fwrite("Conveyor belt starting").
+    io:fwrite("Conveyor belt starting... Proccess: ~p~n", [self()]).
 
 
 start() ->
-    io:fwrite("Hello, world!\n").
+
+    StartPid = self(),
+
+    io:fwrite("Started main function... Proccess: ~p~n", [StartPid]),
+
+    TruckSpawnerPid = spawn(task1, truck_spawner, [StartPid]),
+    
+    receive {done, TruckSpawnerPid } ->
+        io:fwrite("Truck spawner was noticed as finished")
+    end.
