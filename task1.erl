27153e6a497fee3d46dc9cfd826a71a6b4db5c47
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-11-26 19:56:18 +0000

    Basic truck spawner

--- task1.erl
+++ task1.erl
@@ -1,16 +1,34 @@
 -module(task1).
 -export([start/0, truck_spawner/1]).
 
+
+-define (TruckLimit, 30).
+
+spawn_truck(Id) ->
+    io:fwrite("Truck Spawner: Creating truck with id ~w~n", [Id]),
+    timer:sleep(300),
+    if 
+        Id < ?TruckLimit -> spawn_truck(Id + 1);
+        true -> io:fwrite("Truck Spawner: No more trucks~n", [])
+    end.
+
+
+
 truck_spawner(StartPid) ->
 
     TruckSpawnerId = self(),
 
     io:fwrite("Truck spawner starting... Proccess: ~p~n", [TruckSpawnerId]),
-    StartPid ! {done, TruckSpawnerId }.
+
+    spawn_truck(1).
     
-package_creator() ->
+
+
+package_creator() -> 
     io:fwrite("Package creator starting... Proccess: ~p~n", [self()]).
-    
+
+
+
 conveyor_belt() ->
     io:fwrite("Conveyor belt starting... Proccess: ~p~n", [self()]).
 
@@ -21,8 +39,5 @@ start() ->
 
     io:fwrite("Started main function... Proccess: ~p~n", [StartPid]),
 
-    TruckSpawnerPid = spawn(task1, truck_spawner, [StartPid]),
+    TruckSpawnerPid = spawn(task1, truck_spawner, [StartPid]).
     
-    receive {done, TruckSpawnerPid } ->
-        io:fwrite("Truck spawner was noticed as finished")
-    end.
