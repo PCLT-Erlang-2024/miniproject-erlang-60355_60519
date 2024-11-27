d3dbc2f0741e79de48a3f28db8cbf16c174858fd
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-11-27 13:45:59 +0000

    Task1 generalized generator behavior with higher order functions

--- task1.erl
+++ task1.erl
@@ -1,5 +1,5 @@
 -module(task1).
--export([start/0, truck_spawner/1, package_creator/0, conveyor_belt/3]).
+-export([start/0, truck_spawner/0, package_creator/0, conveyor_belt/3]).
 
 
 -define (TruckSpawnDelay, 300).
@@ -30,126 +30,83 @@ broadcast_msg([Pid | PidList], Msg) ->
     broadcast_msg(PidList, Msg).
 
 
+%used to verify if the delay is correct (and maybe other conditions in the future)
+generator_loop_check(GeneratorFunction, GeneratorName, GeneratedEntities, NextId, DelayToGenerate, InitialDelay) ->
+            
+    if 
+        DelayToGenerate =< 0 -> %if in the meantime the wait time has already ended, create immediatly a new entity and continue the loop
+            generator_loop(GeneratorFunction, GeneratorName, [GeneratorFunction(NextId) | GeneratedEntities], NextId + 1, InitialDelay, InitialDelay);
+
+        DelayToGenerate > 0 -> %There is still time to wait
+            generator_loop(GeneratorFunction, GeneratorName, GeneratedEntities, NextId, DelayToGenerate, InitialDelay)
+    end.
+
+
+%when there are no trucks, we don't even receive the requests to return them
+generator_loop(GeneratorFunction, GeneratorName, [], NextId, DelayToGenerate, InitialDelay) ->
+
+    %while waiting to create new package, be open to messages (stop)
+    receive
+        stop ->  
+            io:fwrite("~s: Received message to stop, stopping...~n", [GeneratorName])
+        
+        %create package after time having waited
+        after DelayToGenerate ->
+            generator_loop_check(GeneratorFunction, GeneratorName, [GeneratorFunction(NextId)], NextId + 1, InitialDelay, InitialDelay)
+    end;
+
+
+%the truck spawner loop when trucks is not empty, so it can receive requests to get trucks
+generator_loop(GeneratorFunction, GeneratorName, Entities, NextId, DelayToGenerate, InitialDelay) ->
+   
+    Now = now_in_milli(), %get current time to track change in delay
+
+    %while waiting to create new entity, be open to messages (request and stop)
+    receive
+
+        stop -> io:fwrite("~s: Received message to stop, stopping...~n", [GeneratorName]);
+
+        %received request to receive one of the entities of this generator
+        {request_for_entity, RequesterPid} ->
+
+            io:fwrite("~s: Received request from ~w...~n", [GeneratorName, RequesterPid]),
+            
+            [EntityToGive | RestOfEntities] = Entities, %extract entity to give (we're sure that exist because Entities is not [])
+
+            RequesterPid ! EntityToGive, %send the entity to the requester
+            
+            generator_loop_check(GeneratorFunction, GeneratorName, RestOfEntities, NextId, DelayToGenerate -  (now_in_milli() - Now), InitialDelay) %continue loop without the given entity and waiting the remaining time
+        
+        %create entity after time having waited
+        after DelayToGenerate -> 
+            generator_loop_check(GeneratorFunction, GeneratorName, [GeneratorFunction(NextId) | Entities], NextId + 1, InitialDelay, InitialDelay)
+    end.
+
+
 %generates a truck {Id, capacity, packages}
 generate_truck(Id) ->
     io:fwrite("Truck Spawner: Creating truck with id ~w~n", [Id]),
     {Id, ?TRUCK_CAPACITY, []}.
 
 
-%used to verify if the delay is correct
-truck_spawner_loop_check(Trucks, NextTruckId, DelayToCreateTruck) ->
-            
-    if 
-        DelayToCreateTruck =< 0 -> %in the meantime the wait time has already ended, create immediatly a new truck and continue the loop
-            truck_spawner_loop([generate_truck(NextTruckId) | Trucks], NextTruckId + 1, ?TruckSpawnDelay);
-
-        DelayToCreateTruck > 0 -> %There is still time to wait
-            truck_spawner_loop(Trucks, NextTruckId, ?TruckSpawnDelay)
-    end.
-
-
-%when there are no trucks, we don't even receive the requests to return them
-truck_spawner_loop([], NextTruckId, DelayToCreateTruck) ->
-
-        timer:sleep(DelayToCreateTruck),
-        truck_spawner_loop_check([generate_truck(NextTruckId)], NextTruckId + 1, ?TruckSpawnDelay);
-
-
-%the truck spawner loop when trucks is not empty, so it can receive requests to get trucks
-truck_spawner_loop(Trucks, NextTruckId, DelayToCreateTruck) ->
-   
-    Now = now_in_milli(), %get current time to track change in delay
-
-    %while waiting to create new truck, be open to messages (request)
-    receive
-
-        stop -> io:fwrite("Truck spawner: Received message to stop, stopping...~n", []);
-
-        %received request to have truck
-        {receive_truck_request, RequesterPid} ->
-
-            io:fwrite("Truck spawner: Received request to receive a truck from ~w...~n", [RequesterPid]),
-            
-            [TruckToGive | RestOfTrucks] = Trucks, %extract truck to give (we're sure that exist because Packages is not [])
-
-            RequesterPid ! TruckToGive, %send the truck to the requester
-            
-            truck_spawner_loop_check(RestOfTrucks, NextTruckId + 1, DelayToCreateTruck -  (now_in_milli() - Now)) %continue loop without the given package and waiting the remaining time
-        
-        %create package after time having waited
-        after DelayToCreateTruck -> 
-            truck_spawner_loop_check([generate_truck(NextTruckId) | Trucks], NextTruckId + 1, ?TruckSpawnDelay)
-    end.
-
-
-truck_spawner(ParentPid) ->
+truck_spawner() ->
 
     TruckSpawnerId = self(),
 
-    io:fwrite("Truck spawner starting... Proccess: ~p~n", [TruckSpawnerId]),
+    io:fwrite("Truck spawner: starting... Proccess: ~p~n", [TruckSpawnerId]),
 
-    truck_spawner_loop([], 1, ?TruckSpawnDelay), %start loop of spawning trucks till we have no more
-
-    %There are no more trucks at this point, sending stop message to parent
-    ParentPid ! stop. 
+    generator_loop(fun generate_truck/1, "Truck spawner", [], 1, ?TruckSpawnDelay, ?TruckSpawnDelay).
 
 
 
-%does the package_creator_loop and checks if the delay is already over
-package_creator_loop_check(Packages, NextPackageId, DelayToCreatePackage) ->
-
-    if
-        DelayToCreatePackage =< 0 -> %if the delay was already waited, then simply create a new package
-            package_creator_loop([{NextPackageId}], NextPackageId + 1, ?PackageCreationDelay);
-
-        DelayToCreatePackage > 0 ->
-            package_creator_loop(Packages, NextPackageId, DelayToCreatePackage)
-    
-    end.
-
-%when there are no packages, we don't even receive the requests to return them
-package_creator_loop([], NextPackageId, DelayToCreatePackage) ->
-
-    %while waiting to create new package, be open to messages (stop)
-    receive
-        stop ->  
-            io:fwrite("Package creator: Received message to stop, stopping...~n", [])
-        
-        %create package after time having waited
-        after DelayToCreatePackage ->
-            io:fwrite("Package creator: Creating package with id ~w~n", [NextPackageId]), 
-            package_creator_loop_check([{NextPackageId}], NextPackageId + 1, ?PackageCreationDelay)
-    end;
-
-package_creator_loop(Packages, NextPackageId, DelayToCreatePackage) ->
-   
-    Now = now_in_milli(), %get current time to track change in delay
-
-    %while waiting to create new package, be open to messages (stop or request package)
-    receive
-        stop ->  
-            io:fwrite("Package creator: Received message to stop, stopping...~n", []);
-
-        %received request to have package
-        {receive_package_request, RequesterPid} ->
-
-            io:fwrite("Package creator: Received request to receive a package from ~w...~n", [RequesterPid]),
-            
-            %extract package to give (we're sure that exist because Packages is not [])
-            [PackageToGive | RestOfPackages] = Packages,
-            RequesterPid ! {PackageToGive},
-            package_creator_loop_check(RestOfPackages, NextPackageId + 1, DelayToCreatePackage -  (now_in_milli() - Now)) %continue loop without the given package and waiting the remaining time
-        
-        %create package after time having waited
-        after DelayToCreatePackage ->
-            io:fwrite("Package creator: Creating package with id ~w~n", [NextPackageId]), 
-            package_creator_loop_check([{NextPackageId} | Packages], NextPackageId + 1, ?PackageCreationDelay)
-    end.
+generate_package(Id) ->
+    io:fwrite("Package creator: Creating package with id ~w~n", [Id]),
+    {Id}.
 
 
 package_creator() -> 
     io:fwrite("Package creator: starting... Proccess: ~p~n", [self()]),
-    package_creator_loop([], 1, ?PackageCreationDelay).
+    generator_loop(fun generate_package/1, "Package creator", [], 1, ?PackageCreationDelay, ?PackageCreationDelay).
 
 
 conveyor_belt_with_truck_loop(Id, PackageCreatorId, TruckSpawnerId, {TruckId, TruckCapacity, Packages}) ->
@@ -160,7 +117,7 @@ conveyor_belt_with_truck_loop(Id, PackageCreatorId, TruckSpawnerId, {TruckId, Tr
         TruckCapacity > 0 ->
 
             %Ask for package
-            PackageCreatorId ! {receive_package_request, self()},
+            PackageCreatorId ! {request_for_entity, self()},
 
             receive
                 stop -> io:fwrite("Conveyor belt ~w: Received message to stop, stopping...~n", [Id]);
@@ -185,7 +142,7 @@ conveyor_belt_loop(Id, PackageCreatorId, TruckSpawnerId) ->
 
 
     %ask for truck
-    TruckSpawnerId ! {receive_truck_request, self()},
+    TruckSpawnerId ! {request_for_entity, self()},
 
     receive
         stop -> io:fwrite("Conveyor belt ~w: Received message to stop, stopping...~n", [Id]);
@@ -208,21 +165,17 @@ conveyor_belt(Id, PackageCreatorPid, TruckSpawnerId) ->
 
 start() ->
 
-    StartPid = self(),
+    io:fwrite("Started main function... Proccess: ~p~n", [self()]),
 
-    io:fwrite("Started main function... Proccess: ~p~n", [StartPid]),
-
-
-    TruckSpawnerPid = spawn(?MODULE, truck_spawner, [StartPid]),
+    TruckSpawnerPid = spawn(?MODULE, truck_spawner, []),
 
     PackageCreatorId = spawn(?MODULE, package_creator, []),
 
     %starts and gets ConveyorIds
     ConveyorsIds = [spawn(?MODULE, conveyor_belt, [C, PackageCreatorId, TruckSpawnerPid]) || C <- lists:seq(1, ?N_CONVEYORS)],
 
-    
     timer:sleep(?TIME_TO_RUN), %we wait for a certain time before naturally ending the process
 
-    io:fwrite("Main Function: Time to run has passed (~w milliseconds), ending proccess...~n", [StartPid]),
+    io:fwrite("Main Function: Time to run has passed (~w milliseconds), ending proccess...~n", [?TIME_TO_RUN]),
 
     broadcast_msg([TruckSpawnerPid | [ PackageCreatorId | ConveyorsIds ]], stop). %we tell the remaining proccesses to stop too
